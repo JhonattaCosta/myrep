40cb90c3d9a3a8fc0baceef6218bbbdf19e9a435
Jhonatta Santos <154709449+JhonattaDev@users.noreply.github.com>
2024-02-22 00:50:18 +0000

    Alteração na frese do index.js

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,5 @@
 function Home() {
-    return <h1>Se você não estudar para alcançar o futuro que quer, deve aceitar o futuro que vier.</h1>
+    return <h1>Se você não estudar para alcançar o futuro que quer, deve aceitar o futuro que vier...</h1>
 }
 
 export default Home;
